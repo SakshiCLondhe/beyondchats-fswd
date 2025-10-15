df4e4c4c846f92241b0e08599f74314f08317b32
sakshiLondhe08 <sakshilondhe@deotechsolutions.com>
2025-10-15 13:16:49 +0530

    Vite + React ready for Vercel

--- frontend/beyondchats_frontend/vite.config.js
+++ frontend/beyondchats_frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // This is the folder Vercel will use
-  }
-})
+    outDir: "dist", // Vercel expects this folder
+  },
+});
