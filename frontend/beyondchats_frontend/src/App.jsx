9023289751967f805f7237c306f2ce60aa943557
sakshiLondhe08 <sakshilondhe@deotechsolutions.com>
2025-10-15 13:13:04 +0530

    Vite + React ready for Vercel

--- frontend/beyondchats_frontend/src/App.jsx
+++ frontend/beyondchats_frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/LoginPage";
 import Register from "./pages/Register";
@@ -6,12 +7,14 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
